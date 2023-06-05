70b9cc8fc6553a79a1e296e1193c45ec33a9b8e1
Harsh-Daga <hs108699@gmail.com>
2023-06-05 21:30:44 +0530

    hero element changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <div className='relative z-0'>
+        <div className="relative z-0">
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
